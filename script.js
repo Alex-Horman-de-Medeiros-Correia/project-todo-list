fa498062ce4eea85678761d17571d05261209305
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2022-01-23 18:00:37 -0300

    adicionando de 1 a 8

---
+++ script.js
@@ -0,0 +1,26 @@
+const botao = document.querySelector('#criar-tarefa');
+const inputt = document.querySelector('#texto-tarefa');
+const lista = document.querySelector('#lista-tarefas');
+let elementList = document.querySelector('.elementList');
+
+const addTarefa = () => {
+    const criacaoElemento = document.createElement('li');
+    criacaoElemento.innerText = inputt.value;
+    criacaoElemento.className = 'elementList';
+    lista.appendChild(criacaoElemento);
+    inputt.value = '';
+};
+
+botao.addEventListener('click', addTarefa);
+
+function colorGrey(e) {
+    const selecionarElemento = document.querySelector('.bgColor');
+    if (selecionarElemento) {
+        selecionarElemento.classList.remove('bgColor');
+    }
+    e.target.classList.add('bgColor');
+}
+
+
+lista.addEventListener('click', colorGrey);
+
